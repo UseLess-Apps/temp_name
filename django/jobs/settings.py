bcdd6c13e84c0cdc791f6231af0b83ed78cc8b37
Jayden Arquelada <jayarquelada@gmail.com>
2020-08-19 11:34:40 -0400

    Database setup, and moved files

--- django/jobs/settings.py
+++ django/jobs/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'temp_name',
+        'USER': 'aaa',
+        'PASSWORD': 'password',
+        'HOST': 'psql',
+        'PORT': '5432',
     }
 }
 
--- django/scraper/urls.py
+++ django/scraper/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('newview', views.new_view, name="new_view")
 ]
+

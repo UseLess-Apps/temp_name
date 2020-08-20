4400119448d1394abe07a4a8f43619bf517eb9af
Jayden Arquelada <jayarquelada@gmail.com>
2020-08-20 12:01:09 -0400

    Models set up

--- django/scraper/models.py
+++ django/scraper/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+MAX_LENGTH = 300
+
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=MAX_LENGTH)
+    description = models.CharField(blank=True, max_length=MAX_LENGTH)
+    company = models.CharField(max_length=MAX_LENGTH)
+    posted_date = models.DateField()
+    work_type = models.CharField(max_length=MAX_LENGTH)
+    location = models.CharField(blank=True, max_length=MAX_LENGTH)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+
+    def __str__(self):
+        return f'id: {self.id}, name: {self.name}'
+
+class Job_to_Tag(models.Model):
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
